9f28b3138ca6dc717caa6861d0a78b5d3a22aa64
faycal-gh <wassimaghali2018@gmail.com>
2026-01-08 00:24:47 +0100

    Fix collation type for conflicts table to use utf8mb4_general_ci

--- database/gestion_examens_db.sql
+++ database/gestion_examens_db.sql
@@ -31,7 +31,7 @@ CREATE TABLE `conflicts` (
   `severity` varchar(20) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
